2bfe2e100cd677f9d7529bc46578595f9cd0331b
Gjenio540 <pawelspisz@gmail.com>
2025-02-18 18:27:57 +0100

    Fix cmake and raygui
    + Fix CMakeList and raygui initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,36 @@
-#include "raylib.h"
-#include "raygui.h"
+#define RAYGUI_IMPLEMENTATION
+
+#include <raylib.h>
+#include <raygui.h>
+#include <iostream>
 
 int main()
 {
-    const int screenWidth = 960;
-    const int screenHeight = 560;
-
-    InitWindow(screenWidth, screenHeight, "FFMPEG-GUI");
-    SetExitKey(0);
-
-    bool exitWindow = false;
+    InitWindow(960, 560, "FFmpeg-GUI");
     SetTargetFPS(60);
 
-    while (!exitWindow)
+    //Font font = LoadFont("Inter-VariableFont.ttf");
+    //GuiSetFont(font);
+
+    bool active = false;
+    const char* options = "YUV422;";
+    int option = 0;
+    
+    while (!WindowShouldClose())
     {
-        exitWindow = WindowShouldClose();
         BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("Pixel Format", 10, 10, 24, DARKGRAY);
+        if (GuiDropdownBox({40, 40, 200, 30}, options, &option, active))
+        {
+            active = !active;
+        }
+
         EndDrawing();
     }
-
+    
+    //UnloadFont(font);
     CloseWindow();
     return 0;
 }
